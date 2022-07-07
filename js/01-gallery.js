4d053e7c67c504fb4187f82487742f225e761136
Sergey Gulaga <ry6ens@gmail.com>
2022-07-07 15:49:22 +0300

    add First Task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,69 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+// 1) Вариант решения
+// const makeGalleryCard = ({ preview, original, description } = {}) => {
+//     const galleryLink = document.createElement("a");
+//     galleryLink.classList.add("gallery__link");
+//     galleryLink.href = original;
+
+//     const galleryLi = document.createElement("img");
+//     galleryLi.classList.add("gallery__image");
+//     galleryLi.src = preview;
+//     galleryLi.alt = description;
+
+//     galleryLink.append(galleryLi);
+
+//     return galleryLink;
+// };
+
+// const galleryCardsArr = galleryItems.map((el) => {
+//   return makeGalleryCard(el);
+// });
+
+// galleryList.append(...galleryCardsArr);
+
+// 2) Вариант решения
+const galleryCardsArr = galleryItems
+  .map(
+    (el) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+            <img 
+            class="gallery__image"
+            src="${el.preview}"
+            alt="${el.description}"
+            data-source="${el.original}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+galleryList.innerHTML = galleryCardsArr;
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${target.dataset.source}" width="800" height="600">
+`
+  );
+
+  instance.show();
+
+  galleryList.addEventListener("keydown", (event) => {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+});
